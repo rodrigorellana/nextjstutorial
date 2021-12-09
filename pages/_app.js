643b6e72c2556e0a40af28c4bd308ad6abae17e1
orellanarodrigo <rodrigo.orellana.1982@gmail.com>
2021-12-09 13:24:31 -0300

    basics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+
+export default function Applit({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
