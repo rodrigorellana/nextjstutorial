930c703d17dbafcb8802dd45a09d4054a1fb5ab6
orellanarodrigo <rodrigo.orellana.1982@gmail.com>
2021-12-13 14:40:22 -0300

    try solving the images bug with akamai

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -17,12 +17,12 @@ export default function FirstPost() {
         </Link>
       </h2>
 
-      {/* <Image
+      <Image
         src="/images/vercel.svg" // Route of the image file
         height={144} // Desired size with correct aspect ratio
         width={144} // Desired size with correct aspect ratio
         alt="Your Name"
-      /> */}
+      />
     </Layout>
   );
 }
